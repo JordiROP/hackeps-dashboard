022400dcff82efc00fba53a7c04fa935ad66012f
JordiROP <jordirop@gmail.com>
2019-10-06 23:23:52 +0200

    added some tables to show the information

--- js/users.js
+++ js/users.js
@@ -1,3 +1,4 @@
+$(document).ready(userStats());
 
 function userStats() {
     var db = firebase.firestore();
@@ -12,6 +13,7 @@ function userStats() {
 
 function countShirts(users) {
     var shirts = new Map();
+    var count = 0;
     shirts.set('S', 0);
     shirts.set('M', 0);
     shirts.set('L', 0);
@@ -20,39 +22,80 @@ function countShirts(users) {
     users.forEach(user => {
         shirts.set(user.shirtSize, shirts.get(user.shirtSize)+1);
     });
-    shirts.forEach()
+
+    shirts.forEach(function(value, key) {
+        $("#shirtsList tbody").append(
+            `<tr>
+                <th scope=${count}>1</th>
+                <td>${key}</td>
+                <td>${value}</td>
+            </tr>`);
+    });
 }
 
 function acceptedUserStatus(users) {
     var acceptedUsers = [];
     var pendentUsers = [];
     var deniedUsers = [];
-
+    var countAccepted = 0;
+    var countPendent = 0;
+    var countDenied = 0;
     users.forEach(user => {
         if(user.accepted === "PENDENT") {
-            pendentUsers.push({"name" : user.fullName, "status": "PENDENT"})
+            $("#pendentList tbody").append(
+                `<tr>
+                    <th scope=${countPendent}>1</th>
+                    <td>${user.fullName}</td>
+                    <td>${user.accepted}</td>
+                </tr>`);
+                countPendent++;
+            //pendentUsers.push({"name" : user.fullName, "status": "PENDENT"})
         } else if (user.accepted === "ACCEPTED") {
-            acceptedUsers.push({"name" : user.fullName, "status": "ACCEPTED"})
+            $("#acceptedList tbody").append(
+                `<tr>
+                    <th scope=${countAccepted}>1</th>
+                    <td>${user.fullName}</td>
+                    <td>${user.accepted}</td>
+                </tr>`);
+            countAccepted++;
+            //acceptedUsers.push({"name" : user.fullName, "status": "ACCEPTED"})
         } else {
-            deniedUsers.push({"name" : user.fullName, "status": "DENIED"})
+            $("#declinedList tbody").append(
+                `<tr>
+                    <th scope=${countDenied}>1</th>
+                    <td>${user.fullName}</td>
+                    <td>${user.accepted}</td>
+                </tr>`);
+            countDenied++;
+            //deniedUsers.push({"name" : user.fullName, "status": "DENIED"})
         }
     });
 }
 
 function gdprStatus(users) {
     gdprForUser = [];
-
+    var count=0;
     users.forEach(user => {
-        gdprForUser.push({"name" : user.fullName, "gdpr" : user.gdpr});
+        $("#gdprList tbody").append(
+            `<tr>
+                <th scope=${count}>1</th>
+                <td>${user.fullName}</td>
+                <td>${user.gdpr}</td>
+            </tr>`);
+        count++;
     });
 }
 
 function getAlergies(users) {
-    alergies = [];
+    var count=0;
     users.forEach(user => {
-        if(user.food) { alergies.push(user.food) }
+        if(user.food) {
+            $("#alergiesList tbody").append(
+                `<tr>
+                    <th scope=${count}>1</th>
+                    <td>${user.food}</td>
+                </tr>`);
+            count++;
+        }
     });
-    console.log(alergies);
 }
-
-userStats();
