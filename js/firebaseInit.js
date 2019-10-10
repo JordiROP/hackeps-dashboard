310350f383fd3c1494333621bf6f27312bdefd96
lleidacoders <36126959+lleidacoders@users.noreply.github.com>
2019-10-10 18:30:48 +0200

    Update firebaseInit.js

--- js/firebaseInit.js
+++ js/firebaseInit.js
@@ -1,12 +1,4 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyA0n4ujEDzIuJz1_w5Wg-QAVZ6Ket54oy4",
-  authDomain: "hackeps-2019.firebaseapp.com",
-  databaseURL: "https://hackeps-2019.firebaseio.com",
-  projectId: "hackeps-2019",
-  storageBucket: "hackeps-2019.appspot.com",
-  messagingSenderId: "335539218936",
-  appId: "1:335539218936:web:c0220bb6a4069a846e8e0d"
-};
+var firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
