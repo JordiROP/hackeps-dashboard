f390c9832bc15ba8342c73c69e9b8566cb4a4fb0
Jordi Ricard Onrubia Palacios <jordirop@gmail.com>
2020-10-15 21:17:11 +0200

    Loggins now is done with backend in heroku, introduced tokens in cookies, users methods now working with heroku

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
 $(function () {
-    $("#login").load("../html/login.html",);
+    if (getCookie("token") == "") {
+        $("#login").load("../html/login.html",);
+    } else {
+        load_page();
+    }
 });
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+function load_page() {
+$("div").remove("#login")
+$("body").append("<div id='navbar'></div>");
+$("#navbar").load("../html/navbar.html",);
+$("body").append('<div class="container-fluid">'+
+'<div class="row">' +
+    '<div id="sidebars"></div>' +
+    '<div class="col-10 offset-2" id="main">' +
+        '<div id="users"></div>' +
+    '</div>' +
+'</div>' +
+'</div>');
+$("#sidebars").load("../html/sidebar.html",);
+$("#users").load("../html/users.html",userStats);
+}
